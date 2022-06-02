7503c0eb3fafcaac020c8d520f270cd992c25c5d
Mhmdaris15 <muhammadaris1945@gmail.com>
2022-06-02 15:30:47 +0700

    Add Slick

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
-$('.my-slick').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
+$(document).ready(function(){
+    $('.down-side').slick({
+        dots:true,
+        arrows:true,
+        autoplay:true,
+        autoplayspeed:20,
+        speed:2000
+    })
+
+    $('.my-slick').slick({
+        dots:true,
+        arrows:false,
+        autoplay:true,
+        autoplayspeed:20,
+        speed:1000
+    })
+
+    let menu = document.querySelector('.menu');
+    let menuLogo1 = document.getElementById('menuLogo1');
+    let menuLogo2 = document.getElementById('menuLogo2');
+    let menuLists = document.querySelectorAll('nav-list-menu');
+    let imgButtons = document.querySelectorAll('.right-side .slick-dots li');
+    // let newDesc;
+
+    menuLogo2.addEventListener('click', e => {
+        menu.style.display = 'block';
+    });
+    menuLogo1.addEventListener('click', e => {
+        menu.style.display = 'none';
+    });
+
+    // const descImgButton = ['Camera House', 'Rocket Launcher', 'Person with The Dog', 'Medical Checkup'];
+    // console.log(imgButtons);
+    // for(let i=0; i < descImgButton.length; i++){
+    //     newDesc = document.createElement('p');
+    //     newDesc.innerText = descImgButton[i];
+    //     imgButtons[i].appendChild(newDesc);
+    // }
+
+    // imgButtons.forEach(imgbutton => {
+    //     imgbutton.addEventListener('')
+    // })
+
+
 })
