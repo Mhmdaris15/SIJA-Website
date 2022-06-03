e55850c3ddb976cb45902a942fa1860166f969c2
Mhmdaris15 <muhammadaris1945@gmail.com>
2022-06-03 15:39:44 +0700

    Alumnus Card and Responsive

--- js/script.js
+++ js/script.js
@@ -40,6 +40,41 @@ $(document).ready(function () {
   // imgButtons.forEach(imgbutton => {
   //     imgbutton.addEventListener('')
   // })
+  $('.alumnus-cards').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 });
 
 // FAUZAN SECTION
@@ -85,3 +120,4 @@ const changeSlide = (direction) => {
     activeSlideIndex * sliderHeight
   }px)`;
 };
+
