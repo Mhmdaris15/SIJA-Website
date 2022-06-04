ad9d7e74c707b3f52148fcc6a3787622602d8e5f
fauzanaziz0098 <fauzanaziz395@gmail.com>
2022-06-04 19:53:03 +0700

    fauzan section
    career path

--- js/script.js
+++ js/script.js
@@ -130,6 +130,41 @@ close.onclick = function () {
 
 // * FAUZAN SECTION --------------------------------------------------------------------------------------------
 
+// -----------------------------------------------career path---------------------------------------------------
+$(".career-cards").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+      },
+    },
+    // {
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 2,
+    //     slidesToScroll: 2,
+    //   },
+    // },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
 // --------------------------------------------head of departement-------------------------------------------
 const sliderContainer = document.querySelector(".slider-container");
 
